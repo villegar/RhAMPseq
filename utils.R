f680bb80f42ea81eae417ab7ba11e4d45fcd0273
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-01 20:02:45 -0500

    Adding function load_data, to load raw data

--- utils.R
+++ utils.R
@@ -3,16 +3,34 @@ hex_logo <- function(subplot = "figures/slope.png",
                      h_color = "#000000",
                      h_fill = "#363b74",
                      output = "figures/metapipe.png",
-                     p_color = "#eeeeee") {
+                     p_color = "#eeeeee",
+                     url = "https://github.com/villegar/RhAMPseq") {
   hexSticker::sticker(subplot = subplot, package = "RhAMPseq", 
                       h_color = h_color,  h_fill = h_fill,
                       dpi = dpi,
-                      # l_x = 1.0, l_y = 1.0, spotlight = FALSE, 
                       s_x = 1.0, s_y = .85, s_width = .5,
                       p_x = 1.0, p_y = 1.52, p_size = 6, p_color = p_color,
-                      url = "https://github.com/villegar/RhAMPseq", 
+                      url = url, 
                       u_angle = 30, u_color = p_color, u_size = 1.35,
                       filename = output)
 }
 
-hex_logo(dpi = 500, h_fill = "#4F0924", output = "figures/logo.png")
+load_data <- function(fasta = NULL, hap_geno = NULL, count_mat = NULL) {
+  fasta_data <- NULL
+  hap_data <- NULL
+  count_mat_data <- NULL
+  
+  if(!is.null(fasta)) {
+    fasta_data <- seqinr::read.fasta(fasta)
+  }
+  
+  if(!is.null(hap_geno)) {
+    hap_data <- read.table(hap_geno, header = TRUE) 
+  }
+  
+  if(!is.null(count_mat)) {
+    count_mat_data <- read.table(count_mat, header = TRUE) 
+  }
+  
+  return(list(fasta_data, hap_data, count_mat_data))
+}
