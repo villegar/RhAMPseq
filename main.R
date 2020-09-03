fa3294e9d7e5c79f4d8284fa0eb87529127f4fc5
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-02 21:34:58 -0500

    Adding code to generate a 'pseudo-genetic map'

--- main.R
+++ main.R
@@ -11,7 +11,9 @@ raw <- load_data(fasta = "data/HaplotypeAllele.fasta",
 ################################################################################
 ## Load mapping family genotypes
 mapping_family <- read_excel_col("data/Rhampseq_populations.xlsx", "A")
-mapping_family_short <- unlist(lapply(mapping_family, function(x) gsub("__.*", "", x)))
+mapping_family_short <- unlist(lapply(mapping_family, 
+                                      function(x) gsub("__.*", "", x)))
+mapping_family_short <- unique(mapping_family_short)
 
 ## Extract genotypes from the hap_genotype file
 hap_geno_names <- names(raw$hap_geno)
@@ -21,13 +23,49 @@ hap_geno_names_blank <- hap_geno_names[grepl("*blank*", hap_geno_names)]
 ## Trim 'X' from genotypes [added when importing the data]
 hap_geno_names <- gsub("X*", "", hap_geno_names)
 ## Further trimming of genotypes: 160_271_001__vDNAlon499A03_A01 -> 160_271_001
-hap_geno_names <- unlist(lapply(hap_geno_names, function(x) gsub("__.*", "", x)))
+hap_geno_names <- unlist(lapply(hap_geno_names, 
+                                function(x) gsub("__.*", "", x)))
 
 ## Lookup for mapping_family genotypes
 hap_geno_names_idx <- hap_geno_names %in% mapping_family_short
 ## Extract matching genotypes
 hap_geno_names_sub <- hap_geno_names[hap_geno_names_idx]
 hap_geno_sub <- raw$hap_geno[hap_geno_names_idx]
+
+## Extract locus data
+locus <- raw$hap_geno$Locus
+
+# Start parallel backend
+tictoc::tic("Parallel") # ~12.719 sec (2 CPUs); ~7.923 sec (4 CPUs)
+cores <- parallel::detectCores() - 1
+cpus <- ifelse(cores > 2, 2, 1)
+cl <- parallel::makeCluster(cpus, setup_strategy = "sequential")
+doParallel::registerDoParallel(cl)
+
+# Load binary operator for backend
+`%dopar%` <- foreach::`%dopar%`
+map <- foreach::foreach(i = 1:length(hap_geno_sub), 
+                        .combine = cbind) %dopar% {
+                          unlist(lapply(hap_geno_sub[, i], get_geno))
+                        }
+parallel::stopCluster(cl) # Stop cluster
+tictoc::toc()
+colnames(map) <- names(hap_geno_sub)
+rownames(map) <- locus
+# map <- cbind(Locus = locus, map)
+map_t <- t(map)
+colnames(map_t) <- col(map)
+rownames(map_t) <- gsub("X*", "", rownames(map_t))
+write.csv(map_t, "data/pseudomap.csv")
+# map_t[1:5,1:5]
+
+# tictoc::tic("Serial") # ~21.387 sec
+# map_s <- data.frame(Locus = locus)
+# for(i in 1:length(hap_geno_sub)) {
+#   map_s <- cbind(map_s, unlist(lapply(hap_geno_sub[, i], get_geno)))
+#   colnames(map_s)[i + 1] <- names(hap_geno_sub)[i] # i + 1 column
+# }
+# tictoc::toc()
 
 ################################################################################
 ############################## Reads Count Matrix ##############################
