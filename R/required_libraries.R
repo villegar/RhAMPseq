7ff14e630ba74776bc0439a3d8caec8ba1313341
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-09-13 11:12:03 +0100

    Wrapping code inside a new function, check_libs

--- R/required_libraries.R
+++ R/required_libraries.R
@@ -1,14 +1,24 @@
-libs <- c("doParallel",
-          "foreach",
-          "hexSticker",
-          "knitr", 
-          "parallel",
-          "readxl", 
-          "seqinr",
-          "tictoc") 
-
-out <- lapply(libs, 
-       function(x) {
-         if (!require(x, character.only = TRUE))
-           install.packages(x, dependencies = TRUE)
-       })
+#' Check for required libraries and install if not currently available 
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' check_libs()
+#' }
+check_libs <- function() {
+  libs <- c("doParallel",
+            "foreach",
+            "hexSticker",
+            "knitr", 
+            "parallel",
+            "readxl", 
+            "seqinr",
+            "tictoc") 
+  
+  out <- lapply(libs, 
+                function(x) {
+                  if (!require(x, character.only = TRUE))
+                    install.packages(x, dependencies = TRUE)
+                })
+}
